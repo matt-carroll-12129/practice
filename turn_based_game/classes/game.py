8a87765c3bb060ebd3b2098d7f3590fa158f97d6
Matt <matt.carroll.12129@gmail.com>
2018-01-17 18:38:23 -0500

    update game.py

--- turn_based_game/classes/game.py
+++ turn_based_game/classes/game.py
@@ -24,3 +24,10 @@ class Person:
 
 	def generate_damage(self):
 		return random.randrange(self.atkl, self.atkh)
+
+	def generate_spell_damage(self, i):
+		mgl = self.magic[i]["dmg"] - 5
+		mgh = self.magic[i]["dmg"] + 5
+		return random.randrange(mgl, mgh)
+
+	
